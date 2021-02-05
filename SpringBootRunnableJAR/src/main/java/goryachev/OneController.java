8d368621d9a279542a23b1c8f11e9028340b6895
LevGoryachev <l.goryachev@inbox.ru>
2021-02-05 12:05:00 +0300

    bug fixing

--- SpringBootRunnableJAR/src/main/java/goryachev/OneController.java
+++ SpringBootRunnableJAR/src/main/java/goryachev/OneController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class Controller {
+public class OneController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="Hello SPb!") String name, Model model) {
         model.addAttribute("name", name);
