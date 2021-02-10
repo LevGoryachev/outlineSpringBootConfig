59781165c6759a2a08adc172916605c762fec4c6
LevGoryachev <l.goryachev@inbox.ru>
2021-02-10 11:55:00 +0300

    add index.html, hellopage.html (Universal module)

--- SpringBootUniversal/src/main/java/OneController.java
+++ SpringBootUniversal/src/main/java/OneController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class OneController {
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="Hello SPb!") String name, Model model) {
+    @GetMapping("/page")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="Hello Java Spring Boot!") String name, Model model) {
         model.addAttribute("name", name);
-        return "greeting";
+        return "hellopage";
     }
 }
