1db90d791769b0c2660291fc15d6e6e59723585d
LevGoryachev <l.goryachev@inbox.ru>
2021-02-11 09:22:00 +0300

    change directory (Universal module)

--- SpringBootUniversal/src/main/java/ru/goryachev/outline/universalconfig/OneController.java
+++ SpringBootUniversal/src/main/java/ru/goryachev/outline/universalconfig/OneController.java
@@ -1,3 +1,5 @@
+package ru.goryachev.outline.universalconfig;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
